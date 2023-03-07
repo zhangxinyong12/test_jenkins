ad58aab103670b1cd3716a6af5170f237c481f97
张新永 <13271150671@wo.cn>
2023-03-07 21:44:05 +0800

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ const HomePage: React.FC = () => {
     <PageContainer ghost>
       <div className={styles.container}>
         <Guide name={trim(name)} />
-        <div>这是prod环境2222222222222222</div>
+        <div>这是prod环境23333333333333333333333333333333333</div>
       </div>
     </PageContainer>
   );
